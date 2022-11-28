46f346cf72113252dc32aa995fa434490938afc4
oulespaul <oulespaul@gmail.com>
2022-11-28 09:57:09 +0700

    fix(layout)

--- src/shared/layouts/AppLayout/index.jsx
+++ src/shared/layouts/AppLayout/index.jsx
@@ -3,7 +3,7 @@ import { AppNavBar } from './components/Navbar';
 
 function AppLayout({ children }) {
   return (
-    <div className="bg-[#2f3534] min-h-full">
+    <div className="bg-[#2f3534] h-screen">
       <AppNavBar />
 
       <main className="flex justify-center overflow-scroll">
